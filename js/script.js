67200ff8925a29484bc56e0b1a7e93e1c4f48f65
antdev <antdev1987@gmail.com>
2022-10-04 05:07:42 -0600

    close btn

--- js/script.js
+++ js/script.js
@@ -4,6 +4,12 @@ var modal = document.getElementById('myModal');
 // Get the button that opens the modal
 var btn = document.querySelectorAll('.openPoup');
 
+const close = document.querySelector('.close');
+
+close.addEventListener('click', () => {
+  modal.style.display = 'none';
+});
+
 // When the user clicks the button, open the modal
 btn.forEach((item) => {
   item.onclick = function () {
@@ -12,8 +18,9 @@ btn.forEach((item) => {
 });
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function (event) {
+
+window.addEventListener('click', (event) => {
   if (event.target == modal) {
     modal.style.display = 'none';
   }
-};
+});
